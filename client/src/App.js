f9c236f70699a5439ea04d47c8428360501bb3bb
Chill83 <aymericmaggio@gmail.com>
2022-09-23 17:43:45 +0200

    Logout function + header Navbar + userReducer(Redux)

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import Routes from "./components/Routes/index";
 import { UidContext } from "./components/Appcontext";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
 
 function App() {
   const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -20,7 +23,9 @@ function App() {
         .catch((err) => console.log("No Token"));
     };
     fetchToken();
-  }, []);
+
+    if (uid) dispatch(getUser(uid));
+  }, [uid]);
 
   return (
     <UidContext.Provider value={uid}>
--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
+import Navbar from "../Navbar";
 
 const index = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profil" element={<Profil />}></Route>
