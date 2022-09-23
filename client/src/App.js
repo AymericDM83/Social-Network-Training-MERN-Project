7bad1ad7a19079165a0addf29c55085a45aae86b
Chill83 <aymericmaggio@gmail.com>
2022-09-23 11:29:32 +0200

    Authentification with SignUp & SignIn forms ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Routes from "./components/Routes/index";
+import { UidContext } from "./components/Appcontext";
+import axios from "axios";
 
 function App() {
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => {
+          console.log(res);
+          setUid(res.data);
+        })
+        .catch((err) => console.log("No Token"));
+    };
+    fetchToken();
+  }, []);
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
+    </UidContext.Provider>
   );
 }
 
